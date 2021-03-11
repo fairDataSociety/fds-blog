25667b1e164939fb6888fc5279a98a1210fca365
Igor Shadurin <igor.shadurin@gmail.com>
2021-03-11 19:25:27 +0300

    log body

--- scripts/update-server.js
+++ scripts/update-server.js
@@ -13,13 +13,13 @@ app.use(bodyParser.json());
 
 app.post('/git', (req, res) => {
     try {
+        console.log(req.body);
         if (req.body.hook.config.secret === secret) {
             console.log('Secret is ok');
             res.sendStatus(200);
             return;
         } else {
             console.log('Secret is not ok');
-
         }
     } catch (e) {
         console.log('Error', e);
